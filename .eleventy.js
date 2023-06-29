2e0aa7e634c58b4e1eb1bf89bdc3343df8a99bf3
Chris Stone <contact@chrisstone.dev>
2023-06-28 23:34:19 -0400

    Remove revisions section

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,7 @@
-function nthIndexOf(string, searchString, occurrence) {
-	let index = -1;
-	for (let i = 0; i < occurrence; i++) {
-		index = string.indexOf(searchString, ++index);
-		if (index === -1) return -1;
-	}
-	return index;
-}
-
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('./src/img/');
 	eleventyConfig.addPassthroughCopy('./src/style.css');
 	eleventyConfig.addPassthroughCopy('./src/favicon.ico');
-	eleventyConfig.addFilter('mostRecentRevisions', (post) => {
-		let index = nthIndexOf(post, '<li>', 3);
-		return index < 0 ? post : post.substring(0, index) + '</ul>';
-	});
 
 	const markdownIt = require('markdown-it');
 	const md = markdownIt({html: true, linkify: false})
