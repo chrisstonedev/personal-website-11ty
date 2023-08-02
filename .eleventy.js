9ea9578abb64d4fa3131557f34a0e21a39e22de6
Chris Stone <contact@chrisstone.dev>
2023-08-02 00:29:49 -0400

    Minor adjustments to generate open graph image work

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
-const path = require('path');
 const sharp = require("sharp");
 
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy('./src/img/');
-	eleventyConfig.addPassthroughCopy('./src/style.css');
-	eleventyConfig.addPassthroughCopy('./src/favicon.ico');
+	const inputDirectory = 'src';
+	const outputDirectory = 'public';
+	eleventyConfig.addPassthroughCopy(`./${inputDirectory}/img/`, {dot: false});
+	eleventyConfig.addPassthroughCopy(`./${inputDirectory}/style.css`);
+	eleventyConfig.addPassthroughCopy(`./${inputDirectory}/favicon.ico`);
 
 	const markdownIt = require('markdown-it');
 	const md = markdownIt({html: true, linkify: false, typographer: true})
@@ -19,19 +19,8 @@ module.exports = function (eleventyConfig) {
 		let isoDate = date?.toISOString().substring(0, 10);
 		return isoDate ? `<time datetime="${isoDate}">${isoDate}</time>` : '';
 	});
-	eleventyConfig.addAsyncShortcode("GenerateSocialImage", async (title, slug, description, pageUrl, pageTag) => {
-		const sep = path.sep;
-		let targetPreviewPath = `/img/preview`;
-		const targetDir = path.normalize(`./public${targetPreviewPath}`);
-		const initDir = path.isAbsolute(targetDir) ? sep : '';
-		targetDir.split(sep).reduce((parentDir, childDir) => {
-			const curDir = path.resolve(parentDir, childDir);
-			if (!fs.existsSync(curDir)) {
-				fs.mkdirSync(curDir);
-			}
-			return curDir;
-		}, initDir);
-
+	eleventyConfig.addAsyncShortcode('generateOpenGraphImage', async (title, slug, description, pageUrl, pageTag) => {
+		let openGraphImageOutputDirectory = `img/preview`;
 		let filename = slug || 'index';
 
 		function createTextSvgFromString(textContent, maxCharsPerRow, topY, lineHeight, fontSizeInPt, fontWeight) {
@@ -47,20 +36,17 @@ module.exports = function (eleventyConfig) {
 			});
 			textRows.push(textRow);
 
-			return textRows.reduce((textSvg, row, i) =>
-					textSvg + `<text style="font-family: sans-serif;" x="50" y="${topY + (i * lineHeight)}" fill="#003" font-size="${fontSizeInPt}pt" font-weight="${fontWeight}">${row}</text>`, ''
-			);
+			return textRows.reduce((textSvg, row, i) => textSvg + `<text style="font-family: sans-serif;" x="50" y="${topY + (i * lineHeight)}" fill="#003" font-size="${fontSizeInPt}pt" font-weight="${fontWeight}">${row}</text>`, '');
 		}
 
 		const titleText = createTextSvgFromString(title, 35, 100, 70, 60, 700);
 		const descriptionText = createTextSvgFromString(description, 65, 300, 50, 36, 400);
 
-		let urlText = `<text style="font-family: sans-serif;" x="150" y="590" fill="#00f" font-size="36pt" font-weight="${pageUrl === '/' ? 700 : 400}">chrisstone.dev</text>`;
+		let urlText = '';
 		if (pageUrl !== '/') {
-			let sectionName = '';
-			let underlineLength = 10;
-			sectionName = pageTag || pageUrl?.replaceAll('/', '');
+			let sectionName = pageTag || pageUrl?.replaceAll('/', '');
 			sectionName = sectionName.charAt(0).toUpperCase() + sectionName?.slice(1);
+			let underlineLength = 10;
 			switch (sectionName) {
 				case 'Thoughts':
 					underlineLength = 165;
@@ -75,9 +61,9 @@ module.exports = function (eleventyConfig) {
 					underlineLength = 135;
 					break;
 			}
-			urlText += `<text style="font-family: sans-serif;" x="1150" y="590" fill="#00f" font-size="36pt" font-weight="700" text-anchor="end">${sectionName}</text>`;
-			urlText += `<line style="stroke: fuchsia; stroke-width: 5px;" x1="${1150 - underlineLength}" y1="610" x2="1150" y2="610"/>`;
-			urlText += `<text style="font-family: sans-serif;" x="395" y="590" fill="#00f" font-size="36pt" font-weight="400">/ ${slug}</text>`;
+			urlText = `<text style="font-family: sans-serif;" x="1150" y="590" fill="#00f" font-size="36pt" font-weight="700" text-anchor="end">${sectionName}</text>
+<line style="stroke: fuchsia; stroke-width: 5px;" x1="${1150 - underlineLength}" y1="610" x2="1150" y2="610"/>
+<text style="font-family: sans-serif;" x="395" y="590" fill="#00f" font-size="36pt" font-weight="400">/ ${slug}</text>`;
 		}
 
 		let svg = `<svg width="1200" height="628" viewbox="0 0 1200 628" xmlns="http://www.w3.org/2000/svg">
@@ -86,11 +72,12 @@ module.exports = function (eleventyConfig) {
 	<g>
 		${titleText}
 		${descriptionText}
+		<text style="font-family: sans-serif;" x="150" y="590" fill="#00f" font-size="36pt" font-weight="${pageUrl === '/' ? 700 : 400}">chrisstone.dev</text>
 		${urlText}
 	</g>
 </svg>`;
 
-		const profileImage = await sharp("./src/img/chris-stone-square.png")
+		const profileImage = await sharp(`./${inputDirectory}/img/chris-stone-square.png`)
 				.resize({width: 100, height: 100, fit: 'contain'})
 				.toBuffer();
 
@@ -98,10 +85,10 @@ module.exports = function (eleventyConfig) {
 				.resize(1200, 628)
 				.composite([{input: profileImage, top: 528, left: 0}])
 				.png()
-				.toFile(`${targetDir}/${filename}.png`);
+				.toFile(`./${outputDirectory}/${openGraphImageOutputDirectory}/${filename}.png`);
 
-		return targetPreviewPath + `/${filename}.png`;
+		return `/${openGraphImageOutputDirectory}/${filename}.png`;
 	});
 	eleventyConfig.setLibrary('md', md);
-	return {dir: {input: 'src', output: 'public'}};
+	return {dir: {input: inputDirectory, output: outputDirectory}};
 }
